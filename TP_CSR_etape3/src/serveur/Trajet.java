a794069322f78b3bbc4316b22220ce5914a52dd0
HBocquet <contact@hbocquet.fr>
2014-12-05 18:21:13 +0100

    add resources

---
+++ TP_CSR_etape3/src/serveur/Trajet.java
@@ -0,0 +1,11 @@
+package serveur;
+
+public class Trajet {
+
+	private int idTrain;
+	
+	private char idGareDepart;
+	
+	private char idGareArrivee;
+	
+}
---
+++ TP_CSR_etape3/src/serveur/TrajetListResource.java
@@ -0,0 +1,13 @@
+package serveur;
+
+import org.restlet.resource.ServerResource;
+
+public class TrajetListResource extends ServerResource {
+
+	@Get
+	public void getList()
+	{
+		
+	}
+	
+}
